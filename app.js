d6e9e221a343a79ca335bfcc9a7e4ee142ef3702
loikvokhidov <loik.vokhidov@gmail.com>
2022-12-23 06:34:43 +0700

    add page for work and about

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = [];
-let workItems =  [];
+const items = [];
+const workItems =  [];
 
 app.set("view engine", "ejs");
 
@@ -12,21 +13,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  }; 
-
-  let day = today.toLocaleDateString("en-US", options);
+const day = date.getDate();
 
   res.render("list", {listTitle: day, newListItems: items});
 }); 
 
 app.post("/", function(req, res) {
-    let item = req.body.newItem;
+    const item = req.body.newItem;
 
     if (req.body.list === "Work") {
         workItems.push(item);
